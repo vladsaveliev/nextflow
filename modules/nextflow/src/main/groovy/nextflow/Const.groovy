b728092914f724c49e20be4c7958b04b8d004cdb
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2018-12-27 10:50:37 +0100

    Bump version to 19.0.0.modules-draft2-SNAPSHOT

--- modules/nextflow/src/main/groovy/nextflow/Const.groovy
+++ modules/nextflow/src/main/groovy/nextflow/Const.groovy
@@ -50,12 +50,12 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "19.01.0-edge"
+    static public final String APP_VER = "19.0.0.modules-draft2-SNAPSHOT"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1547117614234
+    static public final long APP_TIMESTAMP = 1545905041641
 
     /**
      * The app build number
