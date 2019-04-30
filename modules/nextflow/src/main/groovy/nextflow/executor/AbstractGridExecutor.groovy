1b7c3e363b1e14904636bdfb42de2e9f20fda2e6
vladsaveliev <vladislav.sav@gmail.com>
2019-05-01 00:06:36 +1000

    Increase wait time for queue status checks

--- modules/nextflow/src/main/groovy/nextflow/executor/AbstractGridExecutor.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/AbstractGridExecutor.groovy
@@ -267,7 +267,7 @@ abstract class AbstractGridExecutor extends Executor {
             final buf = new StringBuilder()
             final process = new ProcessBuilder(cmd).redirectErrorStream(true).start()
             final consumer = process.consumeProcessOutputStream(buf)
-            process.waitForOrKill(60_000)
+            process.waitForOrKill(240_000)
             final exit = process.exitValue(); consumer.join() // <-- make sure sync with the output consume #1045
             final result = buf.toString()
 
